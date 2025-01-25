038c5d59db4ef913f0627dbd0ceffd7eb9ab7a05
harsha919 <95274054+harsha919@users.noreply.github.com>
2025-01-25 20:47:25 +0530

    new tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,10 @@
 "use client";
 
-// import  'Montserrat', sans-serif !important;
 import * as React from "react";
-import { Montserrat } from "next/font/google";
 
 import Hero from "@/components/Hero";
 import SubNav from "@/components/SubNav";
 import HomeContent from "@/components/HomeContent";
-
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"], // Choose the desired font weights
-  variable: "--font-montserrat", // Optional: Create a CSS variable for reusability
-});
 
 export default function Home() {
   return (
